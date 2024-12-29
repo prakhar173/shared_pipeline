97efb1ea710252edfce2e65ed1e2caf519cebcc6
Prakhar Mishra <72335934+prakhar173@users.noreply.github.com>
2024-12-29 17:26:59 +0530

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,8 +1,3 @@
-// src/com/example/MyPipeline.groovy
-package com.example
-
-class MyPipeline {
-    def call() {
-        echo "Hello from the shared pipeline via object!"
-    }
+def call() {
+    echo "Hello from the shared pipeline!"
 }
