2ce7eaba4f5e0156ddf7356d973cde5532d8cfb1
Prakhar Mishra <72335934+prakhar173@users.noreply.github.com>
2025-01-05 23:08:10 +0530

    Update prakhar.groovy

--- vars/prakhar.groovy
+++ vars/prakhar.groovy
@@ -7,39 +7,10 @@ def call() {
         GITHUB_REPO = 'https://github.com/prakhar173/Devops-Assignment-2023mt93305.git'
     }
     stages {
-        stage('Checkout') {
+        stage('Example') {
             steps {
-                checkout([$class: 'GitSCM',
-                          branches: [[name: "${params.BRANCH}"]],
-                          userRemoteConfigs: [[url: "https://github.com/${env.GITHUB_REPO}.git"]]])
+                echo 'Hello World'
             }
-        }
-      stage('git') {
-                git credentialsId: 'd6c43a83-7a80-4fea-a6e4-ba1732a8c771', url: 'https://github.com/prakhar173/Devops-Assignment-2023mt93305.git'
-        }
-      
-      stage('Build') {
-        // Define the Maven executable path
-        def mvnHome = tool 'maven'
-        // Run Maven clean and package commands
-        bat "mvn clean package"        
-        }
-        stage('Test') {
-            steps {
-                echo "Running tests for ${params.APP_NAME}"
-                // Add test steps here
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo "Deploying ${params.APP_NAME}"
-                // Add deploy steps here
-            }
-        }
-    }
-    post {
-        always {
-            echo "Pipeline completed for ${params.APP_NAME}"
         }
     }
 }
